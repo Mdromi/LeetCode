7d41571d609e211aed2aa40e705acd89f883d13a
Mdromi <mdromi14@gmail.com>
2022-11-25 00:40:28 +0600

    2259. Remove Digit From Number to Maximize Result

---
+++ 20. Valid Parentheses/20.valid-parentheses.js
@@ -0,0 +1,16 @@
+/**
+ * node 20.valid-parentheses
+ * @param {string} 
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+
+    for(let i = 0 ; i < s.length; i++) {
+        console.log(s[i]);
+    }
+
+    // console.log(s.length);
+};
+
+const s = "()[]{}"
+isValid(s)
---
+++ 2259. Remove Digit From Number to Maximize Result/2259.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+
+function str(arr, len) {
+    return arr.indexOf(0)
+}
+
+ var removeDigit = function(number, digit) {
+
+    const strlen = number.length;
+    let temp = number;
+    const positiveDigit = [];
+    for (let i = 0; i < strlen; i++) {
+        if(number[i] === digit) {
+            temp = number.slice(0, i) + number.slice(i+1, number.length)
+            positiveDigit.push(temp)
+        }
+    }
+    
+    let bigDigit = positiveDigit[0]
+    for (let i = 1; i < positiveDigit.length; i++) {
+        if(positiveDigit[i] > bigDigit) bigDigit = positiveDigit[i];
+    }
+
+    return bigDigit
+};
+
+var value =  removeDigit ('133235', '3') 
+console.log(value);
