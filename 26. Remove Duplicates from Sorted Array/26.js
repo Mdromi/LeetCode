f16c39c6d3e0fd5d166c6f9096946fa86bb830f9
Mdromi <mdromi14@gmail.com>
2022-11-25 14:41:25 +0600

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/26.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] === nums[i+1]) {
+            nums.splice(i, 1)
+            i--;
+        }
+    }
+    return nums
+};
+
+const v = removeDuplicates([1,1,2]) 
+console.log(v);
